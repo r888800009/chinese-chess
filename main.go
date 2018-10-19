bf5f75b27fb33fa422b48e8d9412e26943ee3c38
r888800009 <r888800009@users.noreply.github.com>
2018-10-19 23:27:38 +0800

    Add chessboard

--- main.go
+++ main.go
@@ -4,18 +4,85 @@ import "github.com/veandco/go-sdl2/sdl"
 
 var surface *sdl.Surface
 var window *sdl.Window
+var render *sdl.Renderer
 const fps = 60
+const size = 60
+const winW, winH = 800, 800
+
+func paintBoard() {
+    render.SetDrawColor(255, 255, 0, sdl.ALPHA_OPAQUE)
+    var centerX, centerY = winW / 2, winH / 2
+
+    // vertical
+    for x := -4; x <= 4; x++ {
+        render.DrawLine(int32(centerX + x * size),
+                        int32(centerY + -4 * size),
+                        int32(centerX + x * size),
+                        int32(centerY + 0 * size))
+
+        render.DrawLine(int32(centerX + x * size),
+                        int32(centerY + 5 * size),
+                        int32(centerX + x * size),
+                        int32(centerY + 1 * size))
+    }
+
+    render.DrawLine(int32(centerX + -4 * size),
+                    int32(centerY + -4 * size),
+                    int32(centerX + -4 * size),
+                    int32(centerY + 5 * size))
+
+    render.DrawLine(int32(centerX + 4 * size),
+                    int32(centerY + -4 * size),
+                    int32(centerX + 4 * size),
+                    int32(centerY + 5 * size))
+
+
+    // horizonatl
+    for y := -4; y <= 5; y++ {
+        render.DrawLine(int32(centerX + -4 * size),
+                        int32(centerY + y * size),
+                        int32(centerX + 4 * size),
+                        int32(centerY + y * size))
+     }
+
+     // X
+     render.DrawLine(int32(centerX + -1 * size),
+                     int32(centerY + 3 * size),
+                     int32(centerX + 1 * size),
+                     int32(centerY + 5 * size))
+
+     render.DrawLine(int32(centerX + 1 * size),
+                     int32(centerY + 3 * size),
+                     int32(centerX + -1 * size),
+                     int32(centerY + 5 * size))
+
+     render.DrawLine(int32(centerX + -1 * size),
+                     int32(centerY + -4 * size),
+                     int32(centerX + 1 * size),
+                     int32(centerY + -2 * size))
+
+     render.DrawLine(int32(centerX + 1 * size),
+                     int32(centerY + -4 * size),
+                     int32(centerX + -1 * size),
+                     int32(centerY + -2 * size))
+}
 
 func display() {
-	surface.FillRect(nil, 0)
-	window.UpdateSurface()
+
+    render.SetDrawColor(0x00, 0x00, 0x00, sdl.ALPHA_OPAQUE)
+    render.Clear()
+
+    paintBoard()
+
+    render.Present()
 }
 
 func main() {
     var err error
 	println("Start window")
-	window, err = sdl.CreateWindow("", sdl.WINDOWPOS_UNDEFINED,
-		sdl.WINDOWPOS_UNDEFINED, 800, 600, sdl.WINDOW_SHOWN)
+
+
+	window, render, err = sdl.CreateWindowAndRenderer(winW, winH, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
